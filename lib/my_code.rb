c69afd4e65602766a57fb644e3095bf49312c654
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-10-17 16:28:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,6 +13,9 @@ end
 def reduce(source_array, starting_point = 0)
   total = starting_point
   i = 0 
+  if source_array[i].is_a? Integer
+    total = source_array[i]
+  end
   while i < source_array.length
     total = yield(total, source_array[i])
     i += 1
