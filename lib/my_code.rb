13661f9ed711cf6ca3b7cb4db26f97a2707ef085
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-10-17 16:25:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,13 @@ def map(source_array)
   end
   return new_array
 end
+
+def reduce(source_array, starting_point = 0)
+  total = starting_point
+  i = 0 
+  while i < source_array.length
+    total = yield(total, source_array[i])
+    i += 1
+  end
+  return total
+end
